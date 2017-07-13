e3e3af727482ec607bd9347f25d232f59a46d6e2
Matthew King <thewmking@gmail.com>
2017-07-13 00:01:31 -0400

    Update migrations for heroku

--- db/migrate/20170615175313_create_attendings.rb
+++ db/migrate/20170615175313_create_attendings.rb
@@ -2,9 +2,11 @@ class CreateAttendings < ActiveRecord::Migration[5.0]
   def change
     create_table :attendings do |t|
       t.references :event,    foreign_key: true
-      t.references :attendee, foreign_key: true
+      t.integer :attendee_id
 
       t.timestamps
     end
+    add_foreign_key :attendings, :users, column: :attendee_id
+    add_index       :attendings, :attendee_id
   end
 end
